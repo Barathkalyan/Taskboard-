d4c9e98bee6d6d36f9d5a5f3b4e0d3c9ddc61fad
Barathkalyan <145141278+Barathkalyan@users.noreply.github.com>
2025-02-11 21:04:13 +0530

    Fixed common bugs in source code

--- taskboard-.py
+++ taskboard-.py
@@ -1,415 +1,200 @@
 #Source code for TasksBoard+
 
 import mysql.connector as mysql 
-mydb=mysql.connect(host="localhost",user="root",passwd="root") 
-#mydb=mysql.connect(host="localhost",user="root",passwd="root",database="Employee") 
-mycursor=mydb.cursor() 
-                        
+
+mydb = mysql.connect(host="localhost", user="root", passwd="root") 
+mycursor = mydb.cursor() 
+
 try:
-    mycursor.execute("Create database Employee") 
-    mycursor.execute("Use employee") 
-    #Creating a table for employee's details: 
-    mycursor.execute("create table emp(Emp_ID int(3) primary key,Name varchar(15),age integer,Designation varchar(120))")
-    mycursor.execute("insert into emp values(100,'ADMIN',52,'Admin'),\ (101,'Bhuvanesh',34,'Jr.Tester'),(102,'Harish',45,'Sr.Tester'),\
-(103,'Rohit',60,'Debugging'),(104,'Tharshan',23,'Developer'),(105,'Roshan',45,'Product Manager'),(106,'Pranav',56,'Project Manager'),\
-(107,'Jagadish',32,'HR Manager'),(108,'Adharv',44,'Data Analyst'),(109,'Siva',31,'Program analyst'),\
-(110,'Barath',50,'Full stack developer')")
-    mydb.commit() 
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS Employee") 
+    mycursor.execute("USE Employee") 
 
-    #Creating table to store employee's tasks:
-    mycursor.execute("Create table Tasks(Emp_Id integer primary key,Taskname varchar(20),Deadline date)") 
-    #Creating table to enter the status of the employee's tasks: 
-    mycursor.execute("Create table Comptasks(Emp_ID integer primary  key,Comp_status varchar(3))")
-except:
-    print()  
+    # Creating a table for employee details
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS emp (
+        Emp_ID INT PRIMARY KEY,
+        Name VARCHAR(15),
+        Age INT,
+        Designation VARCHAR(120)
+    )""")
 
-#Function to add a new Employee: 
-def New_Employee(): 
+    mycursor.execute("""INSERT INTO emp VALUES
+        (100, 'ADMIN', 52, 'Admin'),
+        (101, 'Bhuvanesh', 34, 'Jr.Tester'),
+        (102, 'Harish', 45, 'Sr.Tester'),
+        (103, 'Rohit', 60, 'Debugging'),
+        (104, 'Tharshan', 23, 'Developer'),
+        (105, 'Roshan', 45, 'Product Manager'),
+        (106, 'Pranav', 56, 'Project Manager'),
+        (107, 'Jagadish', 32, 'HR Manager'),
+        (108, 'Adharv', 44, 'Data Analyst'),
+        (109, 'Siva', 31, 'Program analyst'),
+        (110, 'Barath', 50, 'Full stack developer')
+    """)
+    mydb.commit()
 
-    e_id=int(input("Enter Admin ID:")) 
-    if e_id==100:
-        def Emp(): 
-            eid=int(input("Enter Employee's ID:")) 
-            name=input("Enter Employee's name:") 
-            age=int(input("Enter Employee's age:")) 
-            desig=str(input("Enter Employee's designation:")) 
-            return eid,name,age,desig
+    # Creating tables for tasks and task completion
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS Tasks (
+        Emp_ID INT,
+        Taskname VARCHAR(50),
+        Deadline DATE,
+        PRIMARY KEY (Emp_ID, Taskname)
+    )""")
+
+    mycursor.execute("""CREATE TABLE IF NOT EXISTS Comptasks (
+        Emp_ID INT,
+        Taskname VARCHAR(50),
+        Comp_status VARCHAR(3),
+        PRIMARY KEY (Emp_ID, Taskname)
+    )""")
+except mysql.Error as e:
+    print("Database Error:", e)  
+
+# Function to add a new employee
+def New_Employee():
+    e_id = int(input("Enter Admin ID: ")) 
+    if e_id == 100:
+        def Emp():
+            eid = int(input("Enter Employee ID: ")) 
+            name = input("Enter Employee Name: ") 
+            age = int(input("Enter Employee Age: ")) 
+            desig = input("Enter Employee Designation: ") 
+            return eid, name, age, desig
+        
         while True:
-            print("Do you want to add new Employees?") 
-            ch=input("Enter your option (y/n)?") 
-            if ch.lower()!='n' 
-                num=int(input("Enter the number of Employees to be added:")) 
-                for i in range(num):                    
+            ch = input("Do you want to add new employees? (y/n): ").lower()
+            if ch != 'n': 
+                num = int(input("Enter the number of employees to add: ")) 
+                for _ in range(num):                    
                     try:
-                        mycursor.execute("Insert into emp values(%s,%s,%s,%s)",Emp()) 
-                        mydb.commit() 
+                        mycursor.execute("INSERT INTO emp VALUES (%s, %s, %s, %s)", Emp()) 
+                        mydb.commit()
                         print("Details added successfully!") 
-                    except:
-                        print("Given ID is already in use. Please enter a valid ID.") 
+                    except mysql.Error as e:
+                        print("Error:", e) 
             else: 
                 break
     else: 
         print("Invalid Admin ID.") 
-#Function to add a new task for an employee:       
+
+# Function to add a new task for an employee
 def Insert_Tasks(): 
-    e_id=int(input("Enter Admin ID:")) 
-    if e_id==100: 
+    e_id = int(input("Enter Admin ID: ")) 
+    if e_id == 100: 
         def Emp_works():
-            
-            eid=int(input("Enter Employee ID:")) 
-            task=str(input("Enter the task:")) 
-            dline=str(input("Enter the deadline:")) 
-            return eid,task,dline
+            eid = int(input("Enter Employee ID: ")) 
+            task = input("Enter the task: ") 
+            dline = input("Enter the deadline (YYYY-MM-DD): ") 
+            return eid, task, dline
         
         while True:
-            
-            print("Do u want to add tasks?") 
-            ch=input("Enter the option (y/n)?") 
-            if ch.lower()!='n':
-                
-                n=int(input("Enter the number of employees:")) 
-                for i in range(n): 
-
+            ch = input("Do you want to add tasks? (y/n): ").lower()
+            if ch != 'n':
+                n = int(input("Enter the number of employees: ")) 
+                for _ in range(n): 
                     try: 
-
-                        mycursor.execute("Insert into Tasks values(%s,%s,%s)",Emp_works()) 
+                        mycursor.execute("INSERT INTO Tasks VALUES (%s, %s, %s)", Emp_works()) 
                         mydb.commit() 
                         print("Record added.")
-                        
-                    except: 
-
-                        print("Given ID already has a task.")
-                        
+                    except mysql.Error as e:
+                        print("Error:", e)
             else: 
                 break
-            
     else: 
         print("Invalid Admin ID.") 
 
-#Function to update/revamp a task for an employee:
-        
+# Function to update tasks
 def Update_Tasks(): 
+    e_id = int(input("Enter Admin ID: ")) 
+    if e_id == 100:
+        mycursor.execute("SELECT Emp_ID, Taskname FROM Tasks")
+        rec = mycursor.fetchall()
+        print("Available tasks to update:", rec)
 
-    e_id=int(input("Enter Admin ID:")) 
-    if e_id==100:
-        
-        mycursor.execute("Select Emp_ID from tasks")
-        rec=mycursor.fetchall() 
-        print("The ID's available to update are:") 
-        l=[]
-        for i in rec: 
-
-            l.append(list(i))
-            
-        for i in l: 
-
-            print(i,end=' ')
-            
-        print() 
-
-        def update(): 
-
-            eid=int(input("Enter ID:")) 
-            tname=str(input("Enter the task:")) 
-            dline=str(input("Enter Date of submission:")) 
-            adata=[tname,dline,eid]     
-            return adata
+        def update():
+            eid = int(input("Enter Employee ID: ")) 
+            taskname = input("Enter the task: ") 
+            dline = input("Enter new deadline (YYYY-MM-DD): ") 
+            return taskname, dline, eid
         
         while True:
-            
-            print("Do u want to update the tasks?") 
-            ch=input("Enter the option (y/n)?") 
-            if ch.lower()!='n':
-                
-                n=int(input("Enter the number of employees:")) 
-                for i in range(n):
-                    
-                    mycursor.execute("Update tasks set taskname=%s,Deadline=%s where Emp_ID=%s",update()) 
+            ch = input("Do you want to update tasks? (y/n): ").lower()
+            if ch != 'n':
+                n = int(input("Enter the number of employees: ")) 
+                for _ in range(n):
+                    mycursor.execute("UPDATE Tasks SET Taskname=%s, Deadline=%s WHERE Emp_ID=%s", update()) 
                     mydb.commit()
                 print("Records updated.") 
-
             else: 
                 break
-            
     else: 
         print("Invalid Admin ID.") 
 
-#Function to delete the details of an employee:
-        
+# Function to delete an employee
 def Del_Emp():
-    
-    e_id=int(input("Enter Admin ID:")) 
-    mycursor.execute("Select Emp_ID from emp") 
-    rec=mycursor.fetchall() 
-    print("The ID's available to delete are:") 
-    l=[] 
-    for i in rec: 
-
-        if i[0]==100: 
-
-            continue 
-
-        l.append(list(i))
-        
-    for i in l: 
-
-        print(i,end=' ') 
-
-    print() 
-
-    if e_id==100: 
-
+    e_id = int(input("Enter Admin ID: ")) 
+    if e_id == 100:
         while True:
-            
-            print("Do you want to delete details of an employee?") 
-            ch=input("Enter the option (y/n)?") 
-            if ch.lower()!='n':
-                
-                n=int(input("Enter the number of employees to be deleted:")) 
-                for i in range(n): 
-
-                    eid=int(input("Enter ID to delete data:")) 
-                    l=[eid] 
-                    mycursor.execute("Delete from emp where Emp_ID=%s",l) 
-                    mycursor.execute("Delete from tasks where Emp_ID=%s",l) 
+            ch = input("Do you want to delete employee details? (y/n): ").lower()
+            if ch != 'n':
+                eid = int(input("Enter Employee ID to delete: ")) 
+                try:
+                    mycursor.execute("DELETE FROM emp WHERE Emp_ID=%s", (eid,)) 
+                    mycursor.execute("DELETE FROM Tasks WHERE Emp_ID=%s", (eid,)) 
+                    mycursor.execute("DELETE FROM Comptasks WHERE Emp_ID=%s", (eid,))
                     mydb.commit()
-                    
-                print("Records deleted.") 
-
+                    print("Employee record deleted.")
+                except mysql.Error as e:
+                    print("Error:", e)
             else: 
                 break
-            
-    else: 
-
-        print("Invalid Admin ID.") 
-
-#Function to view the Completion of tasks:
-        
-def Task_Comp(): 
-
-    EID=int(input("Enter Admin ID:")) 
-    if EID==100:
-        
-        mycursor.execute("select * from comptasks") 
-        rec=mycursor.fetchall() 
-        if rec==[]: 
-
-            print("Tasks not completed yet.") 
-        else: 
-            print(rec) 
-
     else: 
         print("Invalid Admin ID.") 
 
-#Function to view the tasks:
-        
+# Function to check task completion
+def Task_Comp():
+    e_id = int(input("Enter Admin ID: ")) 
+    if e_id == 100:
+        mycursor.execute("SELECT * FROM Comptasks")
+        rec = mycursor.fetchall() 
+        print("Completed Tasks:", rec if rec else "No tasks completed.")
+    else: 
+        print("Invalid Admin ID.") 
+
+# Employee functions
 def Emp_Login(): 
-
-    def Works():
-        
-        e_id=int(input("Enter your ID:"))
-        l=[e_id] 
-        return l
-    
-    while True:
-        
-        print("Do u want to view the tasks?") 
-        ch=input("Enter the option (y/n):") 
-        if ch.lower()!='n':
-        
-            n=int(input("Enter the no. of employees to view their works:")) 
-            for i in range(n): 
-
-                mycursor.execute("Select * from tasks where Emp_ID=%s",Works()) 
-                rec=mycursor.fetchall() 
-                flag=False 
-                for x in rec:
-                    
-                    print(x) 
-                    flag=True 
-
-                if flag==False:
-                    
-                    print("No tasks") 
-
-        else: 
-
-            break 
-
-#Function to check whether task is completed or not 
+    eid = int(input("Enter your Employee ID: "))
+    mycursor.execute("SELECT * FROM Tasks WHERE Emp_ID=%s", (eid,))
+    rec = mycursor.fetchall()
+    print("Your Tasks:", rec if rec else "No tasks assigned.")
 
 def Emp_com():
-    
     while True: 
-
-        print("Do you want to confirm the submission of your task?") 
-        ch=input("Enter the choice(y/n):") 
-        if ch!='n':
-            
-            eid=int(input("Enter your ID:")) 
-            task=str(input("Please enter your task:")) 
-            comp=str(input("Is the task completed?(YES/NO):")) 
-            a=[eid,task] 
-            l=[eid,comp]
-            
-            try:
-                
-                if comp=='YES': 
-
-                    mycursor.execute("Insert into comptasks values(%s,%s)",l) 
-                    mycursor.execute("Delete from tasks where Emp_ID=%s and Taskname=%s",a)
-                    mydb.commit()
-                    
-                else:
-                    
-                    print("Complete the task soon and update.") 
-
-            except:
-                
-                print("Task Completion already confirmed.") 
-
+        ch = input("Do you want to confirm task completion? (y/n): ").lower()
+        if ch != 'n':
+            eid = int(input("Enter your Employee ID: ")) 
+            task = input("Enter your task: ") 
+            comp = input("Is the task completed? (YES/NO): ") 
+            if comp.upper() == "YES": 
+                mycursor.execute("INSERT INTO Comptasks VALUES (%s, %s, %s)", (eid, task, comp))
+                mycursor.execute("DELETE FROM Tasks WHERE Emp_ID=%s AND Taskname=%s", (eid, task))
+                mydb.commit()
+                print("Task marked as completed.") 
         else: 
-
             break 
 
-#Menu to use the aforementioned functions: 
-
-
-print("_________"*8) 
-
-print("\t\t\t\t - Welcome to Tasksboard+ - ") 
-
-print("_________"*8) 
-
-print("============================") 
-
-print("\t    Admin Mode:") 
-
-print("============================") 
-
-print() 
-
-print("->1. New Enrollment") 
-
-print("->2. Inserting tasks") 
-
-print("->3. Updating tasks") 
-
-print("->4. Disenrollment") 
-
-print("->5. Check Completion Of Tasks") 
-
-print() 
-
-print("============================") 
-
-print("\tEmployee Mode:") 
-
-print("============================") 
-
-print() 
-
-print("->6. Viewing Tasks") 
-
-print("->7. Confirmation of completion of Tasks") 
-
-print("->8. Exit") 
-
-print() 
-
-print('-------------------------------------------------------') 
-
-  
-
+# Main Menu
 while True:
-    
-    ch=int(input("Enter your choice of operation (1-8) :")) 
-    print('-------------------------------------------------------') 
-    if ch==1:
-        
-        print("Enrollment of New Employee......") 
-
-        print('-------------------------------------------------------') 
-
-        New_Employee() 
-
-        print('-------------------------------------------------------') 
-
-    elif ch==2:
-        
-        print("Inserting Task For Employee......") 
-
-        print('-------------------------------------------------------') 
-
-        Insert_Tasks() 
-
-        print('-------------------------------------------------------')
-        
-    elif ch==3: 
-
-        print("Updating Task Of Employee......") 
-
-        print('-------------------------------------------------------') 
-
-        Update_Tasks() 
-
-        print('-------------------------------------------------------') 
-
-    elif ch==4:
-        
-        print("Disenrollment Of Employee......") 
-
-        print('-------------------------------------------------------') 
-
-        Del_Emp() 
-
-        print('-------------------------------------------------------') 
-
-    elif ch==5:
-        
-        print("Viewing The Completion Of Tasks......") 
-
-        print('-------------------------------------------------------') 
-
-        Task_Comp() 
-
-        print('-------------------------------------------------------') 
-
-  
-
-    elif ch==6:
-        
-        print("Viewing The Tasks Of Employees......") 
-
-        print('-------------------------------------------------------') 
-
-        Emp_Login() 
-
-        print('-------------------------------------------------------') 
-
-    elif ch==7:
-        
-        print("Confirming The Completion Of Tasks......") 
-
-        print('-------------------------------------------------------') 
-
-        Emp_com() 
-
-        print('-------------------------------------------------------') 
-
-    elif ch==8:
-        
-        mycursor.execute("Delete from comptasks") 
-
-        mydb.commit() 
-
-        print("_________"*4) 
-
-        print("\t\t\t\t\t   Thank You") 
-
-        print("_________"*4) 
-
-        break 
+    print("1. New Employee\n2. Insert Task\n3. Update Task\n4. Delete Employee\n5. View Task Completion\n6. View Tasks\n7. Confirm Task Completion\n8. Exit")
+    ch = int(input("Enter choice: ")) 
+    if ch == 1: New_Employee()
+    elif ch == 2: Insert_Tasks()
+    elif ch == 3: Update_Tasks()
+    elif ch == 4: Del_Emp()
+    elif ch == 5: Task_Comp()
+    elif ch == 6: Emp_Login()
+    elif ch == 7: Emp_com()
+    elif ch == 8: break
+    else: print("Invalid option.")
 
     else:  
         print("Invalid option. Please select a valid option.") 
